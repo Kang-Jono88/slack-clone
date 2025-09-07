59c7d7eace65641e64c4f02097fbf06c8dc4f093
jono <jojonmojo35@gmail.com>
2025-09-07 08:27:40 +0700

    stream & sentry added

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -1,7 +1,7 @@
 import { Inngest } from "inngest";
 import { connectDB } from "./db.js";
 import { User } from "../models/user.model.js"; // Import the User model
-//import { addUserToPublicChannels, deleteStreamUser, upsertStreamUser } from "./stream.js";
+import { addUserToPublicChannels, deleteStreamUser, upsertStreamUser } from "./stream.js";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "slack-clone" });
@@ -23,13 +23,13 @@ const syncUser = inngest.createFunction(
 
     await User.create(newUser);
 
-    // await upsertStreamUser({
-    //   id: newUser.clerkId.toString(),
-    //   name: newUser.name,
-    //   image: newUser.image,
-    // });
+    await upsertStreamUser({
+      id: newUser.clerkId.toString(),
+      name: newUser.name,
+      image: newUser.image,
+    });
 
-    // await addUserToPublicChannels(newUser.clerkId.toString());
+    await addUserToPublicChannels(newUser.clerkId.toString());
   }
 );
 
@@ -41,7 +41,7 @@ const deleteUserFromDB = inngest.createFunction(
     const { id } = event.data;
     await User.deleteOne({ clerkId: id });
 
-    // await deleteStreamUser(id.toString());
+    await deleteStreamUser(id.toString());
   }
 );
 
